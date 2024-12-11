15c85724d8c28effb20434985d09ade542d2a114
Thomas Noone <tnoone@lcs.on.ca>
2024-12-11 11:32:26 -0500

    Initial Commit

---
+++ TopTenFavouriteThings/TopTenFavouriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopTenFavouriteThingsApp.swift
+//  TopTenFavouriteThings
+//
+//  Created by Thomas Noone on 2024-12-11.
+//
+
+import SwiftUI
+
+@main
+struct TopTenFavouriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
